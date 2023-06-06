6cc35e8037484349ad4b865076615d2dd095bd4a
RaphaelBoas64 <emailgit>
2023-06-06 12:29:32 -0300

    Inicio controle de versão

---
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -0,0 +1,12 @@
+package com.itb.inf2fm.comercio.model;
+
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String codigoBarras;
+	private double preco;
+	
+	
+}
