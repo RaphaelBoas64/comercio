a25772babf162d68c305c50cde2fe2fb14f6d6d6
RaphaelBoas64 <emailgit>
2023-06-14 08:30:16 -0300

    asda

--- src/main/java/com/itb/inf2fm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -2,6 +2,36 @@ package com.itb.inf2fm.comercio.model;
 
 public class Produto {
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
 	private Long id;
 	private String nome;
 	private String descricao;
